e61cd270efb96dbf3884e3f2b360e4aafcfda352
Naetmul <Naetmul@gmail.com>
2022-07-18 17:10:14 +0900

    Make focus of editor not change

--- matlab/getOFDMPreambleAndPilot.m
+++ matlab/getOFDMPreambleAndPilot.m
@@ -2,15 +2,18 @@ function value = getOFDMPreambleAndPilot(varargin)
     if ~exist("getOFDMPreambleAndPilot_imp.m", "file")
         disp("Trying to retrieve getOFDMPreambleAndPilot() implementation...");
 
+        active_document = matlab.desktop.editor.getActive();
+
         current_folder = pwd();
         openExample('comm/OFDMSynchronizationExample','supportingFile','getOFDMPreambleAndPilot.m');
         example_folder = pwd();
-    
+
         document = matlab.desktop.editor.findOpenDocument( ...
             fullfile(example_folder, 'getOFDMPreambleAndPilot.m'));
         document.saveAs(fullfile(current_folder, "getOFDMPreambleAndPilot_imp.m"));
-
+        
         cd(current_folder);
+        active_document.makeActive();
     end
 
     value = getOFDMPreambleAndPilot_imp(varargin{:});
